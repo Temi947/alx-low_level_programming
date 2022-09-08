fb8e58f762ab0cf2add7ee3701d3b9fef3b21792
Temi947 <adebayotemitope947@gmail.com>
2022-09-08 12:44:37 -0700

    low progrmming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,13 @@
  */
 int main(void)
 {
-	char a; 
-	int b; 
-	long int c; 
-	long long int d;
-	float e; 
+	Size of a char: 1 byte(s)
+	Size of an int: 4 byte(s)
+	Size of a long int: 8 byte(s)
+	Size of a long long int: 8 byte(s)
+	Size of a float: 4 byte(s)
+
+
 
 	printf("Size of a char: 1 byte(s)\n", sizeof(a));
 	printf("Size of an int: 4 byte(s)\n", sizeof(b));
