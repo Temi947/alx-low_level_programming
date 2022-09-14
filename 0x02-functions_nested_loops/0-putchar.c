f0a1f555faa3a0225e308392a71f8c8dd23bb265
Temi947 <adebayotemitope947@gmail.com>
2022-09-13 20:42:28 -0700

    c8functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "putchar.h"
+#include "main.h"
 
 /**
  * main - prints _putchar, followed by a new line.
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
