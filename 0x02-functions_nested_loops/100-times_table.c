3852eeaee5f5ef5067c4a09d926a87fb44a07aa7
Temi947 <adebayotemitope947@gmail.com>
2022-09-14 13:25:04 -0700

    lowe

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,49 +3,45 @@
 /**
  * print_times_table - prints times table
  * @n : times table to use
- * Return:void
  */
-
 void print_times_table(int n)
 {
+	int i, j, k;
 
-	int a = 0, rep, b;
-
-	if (n < 0 || n > 15)
-		return;
-
-	while (a <= n)
+	if (n >= 0 && n <= 15)
 	{
-		for (b = 0; b <= n; b++)
+		for (i = 0; i <= n; i++)
 		{
-			rep = a * b;
-			if (b == 0)
-				_putchar('0' + rep);
-			else if (rep < 10)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar((k / 100) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			else if (rep < 100)
-			{
-				_putchar(' ');
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
-			else
-			{
-				_putchar('0' + rep / 100);
-				_putchar('0' + (rep - 100) / 10);
-				_putchar('0' + rep % 10);
-			}
-			if (b < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-		a++;
 	}
 }
