af6c6b7fcb8be8ab5f4701b460104056bc2d5c4e
Temi947 <adebayotemitope947@gmail.com>
2022-09-14 11:11:20 -0700

    lowe

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print list of alphabets 10 times
+ * print_alphabet_x10 - print alphabets 10 times
  * 
- * Return: 0
+ * Return: void
  */
 
 void print_alphabet_x10(void)
