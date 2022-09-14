f0670a8f00d034adc126573991e77f63d06016c9
Temi947 <adebayotemitope947@gmail.com>
2022-09-14 11:16:52 -0700

    lowe

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,18 @@
 
 /**
  * print_alphabet -  use only user defined variables to create output
- *
- * Description: Write program to print "a to z" using _putchar
  * Return: always success
  */
-int main(void)
+
+void print_alphabet(void)
 {
 	char c = 'a';
 
 	while (c <= 'z')
 	{
-
 		_putchar(c);
 		c++;
+		
 	}
 	_putchar('\n');
 
