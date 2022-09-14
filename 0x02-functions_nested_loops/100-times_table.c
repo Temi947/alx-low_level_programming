62beeaf68ae92dc4f7f0e6b9646d83388743d8e1
Temi947 <adebayotemitope947@gmail.com>
2022-09-14 13:00:16 -0700

    lowe

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,6 @@ void print_times_table(int n)
 
 	int a = 0, rep, b;
 
-
 	if (n < 0 || n > 15)
 		return;
 
@@ -23,6 +22,12 @@ void print_times_table(int n)
 			if (b == 0)
 				_putchar('0' + rep);
 			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (rep < 100)
 			{
 				_putchar(' ');
 				_putchar('0' + rep / 10);
