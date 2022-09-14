f996e7093937ed067aebe91e188e952acc064f99
Temi947 <adebayotemitope947@gmail.com>
2022-09-14 11:12:46 -0700

    lowe

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet_x10 - print alphabets 10 times
- * 
  * Return: void
  */
 
@@ -16,6 +15,7 @@ void print_alphabet_x10(void)
 		c = 'a';
 		while (c <= 'z')
 		{
+
 			_putchar(c);
 			c++;
 		}
