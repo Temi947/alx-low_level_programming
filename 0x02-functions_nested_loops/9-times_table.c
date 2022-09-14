b8d36a6449193c0bb62146fcbb35d0cf3139a38a
Temi947 <adebayotemitope947@gmail.com>
2022-09-14 11:48:49 -0700

    lowe

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,14 +21,14 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-		_putchar(' ')
-		_putchar(k + '0');
+			_putchar(' ');
+			_putchar(k + '0');
 		} else if (k >= 10)
 		{
-		_putchar(',');
-	_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
 		}
 		}
 		_putchar('\n');
