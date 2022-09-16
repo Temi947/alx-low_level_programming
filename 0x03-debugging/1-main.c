f7196a4b1ce022be9e5291f0a838d024522686b4
Temi947 <adebayotemitope947@gmail.com>
2022-09-16 14:01:41 -0700

    lowe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,14 +12,14 @@ int main(void)
 	 printf("Infinite loop incoming :(\n");
 
 	 i = 0;
-
-	 while (1 < 10)
-	 {
-		 putchar(i);
-	 }
-
+	 /*
+	  * while (i < 10)
+	  * {
+	  * putchar(i);
+	  * }
+	  */
 	 printf("Infinite loop avoided! \\o/\n");
 
 	 return (0);
 }
-	
+
