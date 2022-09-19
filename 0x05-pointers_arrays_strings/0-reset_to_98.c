e2c0346810e45a9992ee93fdec3d5bff528c4fc5
Temi947 <adebayotemitope947@gmail.com>
2022-09-18 23:59:48 -0700

    c-arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * reset_to_98 - function that takes a pointer to an integer parameter
+ * @n: integer pointer
+ * Return: 0 Success
  */
-int main(void)
-{
-	int n;
 
-	 n = 402;
-	 printf("n=%d\n", n);
-	 reset_to_98(&n);
-	 printf("n=%d\n", n);
-	 return (0);
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
