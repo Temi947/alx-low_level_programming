133432a9cd010c6713ac8ed31848e14ee939f435
Temi947 <adebayotemitope947@gmail.com>
2022-09-23 10:14:10 -0700

    c-more

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - You are not allowed to use the variable a in your new line of code
+	 * - You are not allowed to modify the variable p
+	 * - You can only write one statement
+	 *   You are not allowed to code anything else than the line of expected line of code at the expected line
+	 */
+	p[5] = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
