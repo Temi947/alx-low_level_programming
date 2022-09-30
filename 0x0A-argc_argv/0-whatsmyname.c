84ab5a8155298fc1bd6bdcfd01c35f5683c50b79
Temi947 <adebayotemitope947@gmail.com>
2022-09-30 10:22:41 -0700

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  * main - prints its name, followed by a new line.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * Return: 0 success.
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
